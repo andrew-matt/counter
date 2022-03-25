264819b089cd2985395dbd2911d6e0435435569d
andrew-matt <andrew.matt@ya.ru>
2022-03-25 17:41:35 +0300

    create numberState, increaseNumber, resetNumber

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Counter} from "./Components/Counter/Counter";
 
 function App() {
+
+    const [number, setNumber] = useState(0)
+
+    const increaseNumber = () => {
+        setNumber(number + 1)
+    }
+
+    const resetNumber = () => {
+        setNumber(0)
+    }
+
     return (
         <div className="App">
-            <Counter/>
+            <Counter
+                number={number}
+                increaseNumber={increaseNumber}
+                resetNumber={resetNumber}
+            />
         </div>
     )
 }
--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import {Button} from "../Button/Button";
 
 type CounterPropsType = {
-
+    number: number
+    increaseNumber: () => void
+    resetNumber: () => void
 }
 
 export const Counter = (props: CounterPropsType) => {
     return (
         <div>
-            0
-            <Button title={"Set"} callback={() => {}}/>
-            <Button title={"Reset"} callback={() => {}}/>
+            {props.number}
+            <Button title={"Inc"} callback={props.increaseNumber}/>
+            <Button title={"Reset"} callback={props.resetNumber}/>
         </div>
     );
 };
